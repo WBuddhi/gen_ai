fabfaa654f67dad1010d470fd328dec838af2fe0
WBuddhi <w.deseram@gmail.com>
2023-12-22 22:48:54 +0530

    Log input parameter for testing

--- src/ingestion/tasks/llama2_source_to_bronze.py
+++ src/ingestion/tasks/llama2_source_to_bronze.py
@@ -4,6 +4,7 @@ from src.ingestion.transformations.to_bronze.llama2.source_to_bronze import (
 )
 
 some_input = db_client.dbutils.widgets.get["some_input"]
+logger.info(f"Input parameter: {some_input}")
 
 source_to_bronze = SourceToBronze(config_path = "../../pipeline_configs/llama2_7b_32k_slr.yaml")
 source_to_bronze.run()
