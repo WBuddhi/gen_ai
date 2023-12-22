9f8a56bccd1328a407b82cfe9ac8a31e0f3c3268
WBuddhi <w.deseram@gmail.com>
2023-12-23 00:13:27 +0530

    Tmp fix bug

--- src/config/spark_config.py
+++ src/config/spark_config.py
@@ -7,7 +7,7 @@ import os
 
 
 def get_spark_session_db_client() -> Tuple[SparkSession, WorkspaceClient]:
-    if os.environ.get("LOGGER_PATH_FROM_ROOT", None):
+    if os.environ.get("REPO_ROOT_PATH", None):
         builder = SparkSession.builder
     else:
         databricks_connect_config = load_yaml(
