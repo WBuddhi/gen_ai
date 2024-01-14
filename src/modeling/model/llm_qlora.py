54db7d9905463d7ba2ddfd3fc4161093af049701
WBuddhi <w.deseram@gmail.com>
2024-01-14 09:12:59 +0100

    Update LlmQlora model

--- src/modeling/model/llm_qlora.py
+++ src/modeling/model/llm_qlora.py
@@ -1,25 +1,35 @@
-import mlflow
 from typing import List, Dict, Union
 import torch
-from peft import PeftModel
+from peft import PeftModel, PeftConfig
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from huggingface_hub import snapshot_download
 
 
-class LlmQlora(mlflow.pyfunc.PythonModel):
-    def load_context(self, context):
+class LlmQlora:
+    def __init__(self, model_path):
+        lora = model_path
+        config = PeftConfig.from_pretrained(model_path)
+        repository = snapshot_download(repo_id=config.base_model_name_or_path)
+        self.artifacts = {
+            "repository": repository,
+            "lora": lora,
+        }
+        self.load_context()
+
+    def load_context(self):
         self.tokenizer = AutoTokenizer.from_pretrained(
-            context.artifacts["repository"]
+            self.artifacts["repository"]
         )
         self.tokenizer.pad_token = self.tokenizer.eos_token
         base_model = AutoModelForCausalLM.from_pretrained(
-            context.artifacts["repository"],
+            self.artifacts["repository"],
             return_dict=True,
             load_in_4bit=True,
             device_map={"": 0},
             trust_remote_code=True,
         )
         self.model = PeftModel.from_pretrained(
-            base_model, context.artifacts["lora"]
+            base_model, self.artifacts["lora"]
         )
 
     def format_prompt(self, article: str) -> str:
@@ -36,20 +46,11 @@ class LlmQlora(mlflow.pyfunc.PythonModel):
             " and NO bullet points, just a long text"
         )
         instruction = "Create a plain language summary for this scientific article provided"
-        instruction_prompt = """[INST]User: {instruction}
+        instruction_prompt = f"""[INST]User: {instruction}
         #ARTICLE:
         {article}
-        [/INST]""".format(
-            instruction="{instruction}", article="{article}"
-        )
-        full_prompt = "\n".join(
-            [
-                system_prompt,
-                instruction_prompt.format(
-                    article=article, instruction=instruction
-                ),
-            ]
-        )
+        [/INST]"""
+        full_prompt = "\n".join([system_prompt, instruction_prompt])
         return full_prompt
 
     def batch_input(self, articles: List[str], batch_size: int) -> List[str]:
@@ -60,31 +61,27 @@ class LlmQlora(mlflow.pyfunc.PythonModel):
         ]
 
     def extract_output_texts(self, generated_texts: List[str]) -> List[str]:
-        output_start_marker = "[/INST]/n"
-        output_end_marker = "</s>"
+        output_start_marker = "[/INST]"
         output_texts = []
         for output in generated_texts:
+            print("cleaning text\n", output)
             start = output.index(output_start_marker) + len(
                 output_start_marker
             )
-            try:
-                end = output.index(output_end_marker)
-                output_texts.append(output[start:end])
-            except ValueError:
-                output_texts.append(output[start:])
+            output_texts.append(output[start:])
+        return output_texts
 
     def predict(
         self,
-        context: Dict[str, str],
         model_input: Dict[str, Union[bool, int, List[str]]],
     ) -> List[str]:
         articles = model_input["articles"]
         temperature = model_input.get("temperature", 1.0)
         max_tokens = model_input.get("max_tokens", 610)
-        top_p = model_input("top_p", 0.7)
-        num_return_sequences = model_input("num_return_sequences", 1)
-        do_sample = model_input("do_sample", True)
-        batch_size = model_input("batch_size", 1)
+        top_p = model_input.get("top_p", 0.7)
+        num_return_sequences = model_input.get("num_return_sequences", 1)
+        do_sample = model_input.get("do_sample", True)
+        batch_size = model_input.get("batch_size", 1)
         batched_prompts = self.batch_input(articles, batch_size)
         outputs = []
         for prompts in batched_prompts:
@@ -102,8 +99,8 @@ class LlmQlora(mlflow.pyfunc.PythonModel):
                     pad_token_id=self.tokenizer.eos_token_id,
                     eos_token_id=self.tokenizer.eos_token_id,
                 )
-            generated_texts = self.tokenizer.decode(
-                generated_ids, skip_special_tokens=False
+            generated_texts = self.tokenizer.batch_decode(
+                generated_ids, skip_special_tokens=True
             )
-            outputs.extend(self.extend_output_texts(generated_texts))
+            outputs.extend(self.extract_output_texts(generated_texts))
         return outputs
